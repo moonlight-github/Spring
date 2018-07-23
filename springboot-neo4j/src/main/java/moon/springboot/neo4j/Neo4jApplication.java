3ed5ec6d2b6bdbba29dea1b359529421e43059c4
moonlight_gu <534064620@qq.com>
2018-07-23 22:13:31 +0800

    新建工程

---
+++ springboot-neo4j/src/main/java/moon/springboot/neo4j/Neo4jApplication.java
@@ -0,0 +1,18 @@
+package moon.springboot.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+* @author 作者 moonlight 
+* @version 创建时间：2018年7月23日 下午10:04:38 
+* 类说明 :
+*/
+
+@SpringBootApplication
+public class Neo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jApplication.class, args);
+	}
+}
